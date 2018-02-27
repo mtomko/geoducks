184037cbde03756050cd8dbcf523023c494e7312
Mark Tomko <mark.tomko.1@gmail.com>
2018-02-26 21:26:03 -0800

    more fastq work

--- core/src/test/scala/org/marktomko/geoducks/stream/StreamOpsTest.scala
+++ core/src/test/scala/org/marktomko/geoducks/stream/StreamOpsTest.scala
@@ -9,7 +9,7 @@ class StreamOpsTest extends FlatSpec with Matchers {
   "grouped" should "group a stream" in {
     val s = Stream.range(1, 9)
     s.grouped(4).toList should be(List(Vector(1, 2, 3, 4), Vector(5, 6, 7, 8)))
-    s.through(grouped(4)).toList should be(List(Vector(1, 2, 3, 4), Vector(5, 6, 7, 8)))
+    s.through(pipe.grouped(4)).toList should be(List(Vector(1, 2, 3, 4), Vector(5, 6, 7, 8)))
   }
 
 }
